2ea366161dab4b6374f0d3f24c5b4baa108403c7
Heringson <majorlazerhs@gmail.com>
2025-10-28 22:24:21 -0300

    Refactor music insertion function and database setup

--- src/musica.ts
+++ src/musica.ts
@@ -1,5 +1,3 @@
-// Gabriel Caire Nomura       2501980   
-// Heitor Matos da Silva      2520912   
 // Heringson Lima             2404307   
 // Rafael Ienne Manoel        2519853   
 // Wesley da Silva Santos     2522594
@@ -120,4 +118,5 @@ async function inserirMusica(): Promise<void> {
 
 // Chama a função principal para iniciar o cadastro
 inserirMusica();
+
 
